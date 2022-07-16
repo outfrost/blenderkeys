9ea24a14e6289630313e632506cd523613807453
L1Q <chuvik.hacker@gmail.com>
2022-07-16 21:06:13 +0300

    Reduce hate
    <3

--- index.js
+++ index.js
@@ -46,19 +46,11 @@ window.onload = () => {
 	let section_template = document.getElementById("section-template");
 	let keybind_template = document.getElementById("keybind-template");
 
-	let sections = Object.keys(content);
-	for (i in sections) {
-		let section_title = sections[i]; // i hate this
-		let section = content[section_title];
-
+	for (const [section_title, section] of Object.entries(content)) {
 		let section_node = section_template.content.cloneNode(true);
 		section_node.getElementById("section-title").textContent = section_title;
 
-		let keybinds = Object.keys(section);
-		for (k in keybinds) {
-			let keybind_label = keybinds[k]; // yep
-			let keybind_keys = section[keybind_label];
-
+		for(const [keybind_label, keybind_keys] of Object.entries(section)) {
 			let keybind_node = keybind_template.content.cloneNode(true);
 			keybind_node.getElementById("label").textContent = keybind_label;
 			keybind_node.getElementById("keys").innerHTML = keybind_keys
